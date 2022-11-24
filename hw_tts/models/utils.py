431132cdaa85ef370a26259a4e1bbc4d0c7c159e
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2022-11-24 15:41:48 +0300

    batch of structured repo changes

---
+++ hw_tts/models/utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_sinusoid_encoding_table(n_position, d_hid, padding_idx=None):
+    ''' Sinusoid position encoding table '''
+
+    def cal_angle(position, hid_idx):
+        return position / np.power(10000, 2 * (hid_idx // 2) / d_hid)
+
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_hid)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(pos_i)
+                               for pos_i in range(n_position)])
+
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2]) 
+
+    if padding_idx is not None:
+        sinusoid_table[padding_idx] = 0.
+
+    return torch.FloatTensor(sinusoid_table)
+
+
+def create_alignment(base_mat, duration_predictor_output):
+    N, L = duration_predictor_output.shape
+    for i in range(N):
+        count = 0
+        for j in range(L):
+            for k in range(duration_predictor_output[i][j]):
+                base_mat[i][count+k][j] = 1
+            count = count + duration_predictor_output[i][j]
+    return base_mat
+
+
+class Transpose(nn.Module):
+    def __init__(self, dim_1, dim_2):
+        super().__init__()
+        self.dim_1 = dim_1
+        self.dim_2 = dim_2
+
+    def forward(self, x):
+        return x.transpose(self.dim_1, self.dim_2)
+
+
+def get_non_pad_mask(seq, pad):
+    assert seq.dim() == 2
+    return seq.ne(pad).type(torch.float).unsqueeze(-1)
+
+
+def get_attn_key_pad_mask(seq_k, seq_q, pad):
+    ''' For masking out the padding part of key sequence. '''
+    # Expand to fit the shape of key query attention matrix.
+    len_q = seq_q.size(1)
+    padding_mask = seq_k.eq(pad)
+    padding_mask = padding_mask.unsqueeze(
+        1).expand(-1, len_q, -1)  # b x lq x lk
+
+    return padding_mask
