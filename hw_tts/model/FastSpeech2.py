d84718abe9b382c8ff031f97f6f3d90f9ae2e727
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2022-11-25 17:06:44 +0300

    second batch of repo construction

---
+++ hw_tts/model/FastSpeech2.py
@@ -0,0 +1,149 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import layers
+import utils
+
+
+
+class LengthRegulator(nn.Module):
+    """ Length Regulator """
+
+    def __init__(self, model_config):
+        super(LengthRegulator, self).__init__()
+        self.duration_predictor = layers.DurationPredictor(model_config['duration'])
+        self.pitch_predictor = layers.DurationPredictor(model_config['pitch'])
+        self.energy_predictor = layers.DurationPredictor(model_config['energy'])
+
+    def LR(self, x, duration_predictor_output, mel_max_length=None):
+        expand_max_len = torch.max(
+            torch.sum(duration_predictor_output, -1), -1)[0]
+        alignment = torch.zeros(duration_predictor_output.size(0),
+                                expand_max_len,
+                                duration_predictor_output.size(1)).numpy()
+        alignment = utils.create_alignment(alignment,
+                                     duration_predictor_output.cpu().numpy())
+        alignment = torch.from_numpy(alignment).to(x.device)
+
+        output = alignment @ x
+        if mel_max_length:
+            output = F.pad(
+                output, (0, 0, 0, mel_max_length-output.size(1), 0, 0))
+        return output
+
+    def forward(self, x, alpha=1.0, target=None, mel_max_length=None):
+        ### Your code here
+        duration_predictor_output = self.duration_predictor(x)
+
+        if target is not None:
+            output = self.LR(x, target, mel_max_length)
+            return output, duration_predictor_output
+        else:
+            output = self.LR(x, (duration_predictor_output * alpha + 0.5).int(), mel_max_length)
+            return output
+
+
+
+class Encoder(nn.Module):
+    def __init__(self, model_config):
+        super(Encoder, self).__init__()
+        
+        len_max_seq=model_config.max_seq_len
+        n_position = len_max_seq + 1
+        n_layers = model_config.encoder_n_layer
+        self.pad = model_config.pad
+
+        self.src_word_emb = nn.Embedding(
+            model_config.vocab_size,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD
+        )
+
+        self.position_enc = nn.Embedding(
+            n_position,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD
+        )
+
+        self.layer_stack = nn.ModuleList([layers.FFTBlock(
+            model_config.encoder_dim,
+            model_config.encoder_conv1d_filter_size,
+            model_config.encoder_head,
+            model_config.encoder_dim // model_config.encoder_head,
+            model_config.encoder_dim // model_config.encoder_head,
+            dropout=model_config.dropout
+        ) for _ in range(n_layers)])
+
+    def forward(self, src_seq, src_pos, return_attns=False):
+
+        enc_slf_attn_list = []
+
+        # -- Prepare masks
+        slf_attn_mask = utils.get_attn_key_pad_mask(seq_k=src_seq, seq_q=src_seq, pad=self.pad)
+        non_pad_mask = utils.get_non_pad_mask(src_seq, self.pad)
+        
+        # -- Forward
+        enc_output = self.src_word_emb(src_seq) + self.position_enc(src_pos)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(
+                enc_output,
+                non_pad_mask=non_pad_mask,
+                slf_attn_mask=slf_attn_mask)
+            if return_attns:
+                enc_slf_attn_list += [enc_slf_attn]
+        
+
+        return enc_output, non_pad_mask
+
+
+class Decoder(nn.Module):
+    """ Decoder """
+
+    def __init__(self, model_config):
+
+        super(Decoder, self).__init__()
+
+        len_max_seq=model_config.max_seq_len
+        n_position = len_max_seq + 1
+        n_layers = model_config.decoder_n_layer
+        self.pad = model_config.pad
+
+        self.position_enc = nn.Embedding(
+            n_position,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD,
+        )
+
+        self.layer_stack = nn.ModuleList([layers.FFTBlock(
+            model_config.encoder_dim,
+            model_config.encoder_conv1d_filter_size,
+            model_config.encoder_head,
+            model_config.encoder_dim // model_config.encoder_head,
+            model_config.encoder_dim // model_config.encoder_head,
+            dropout=model_config.dropout
+        ) for _ in range(n_layers)])
+
+    def forward(self, enc_seq, enc_pos, return_attns=False):
+
+        dec_slf_attn_list = []
+
+        # -- Prepare masks
+        slf_attn_mask = utils.get_attn_key_pad_mask(seq_k=enc_pos, seq_q=enc_pos, pad=self.pad)
+        non_pad_mask = utils.get_non_pad_mask(enc_pos, self.pad)
+
+        # -- Forward
+        dec_output = enc_seq + self.position_enc(enc_pos)
+
+        for dec_layer in self.layer_stack:
+            dec_output, dec_slf_attn = dec_layer(
+                dec_output,
+                non_pad_mask=non_pad_mask,
+                slf_attn_mask=slf_attn_mask)
+            if return_attns:
+                dec_slf_attn_list += [dec_slf_attn]
+
+        return dec_output
+    
---
+++ hw_tts/trainer.py
@@ -0,0 +1,98 @@
+import torch
+from torch.optim.lr_scheduler  import OneCycleLR
+import hw_tts.model, hw_tts.loss, hw_tts.dataset
+from logger import WanDBWriter
+from tqdm.auto import tqdm
+import os
+
+
+class Trainer():
+    def __init__(self, config):
+        self.config = config
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.dataloader = hw_tts.dataset.get_LJSpeech_dataloader(config['dataset'])
+
+        self.model = getattr(hw_tts.model, config['model']['type'])(**dict(config['model']['args']))
+        self.model = self.model.to(self.device)
+
+        self.Loss = getattr(hw_tts.loss, config['loss'])()
+
+        self.optimizer = torch.optim.AdamW(
+            self.model.parameters(),
+            lr=config['trainer']['learning_rate'],
+            weight_decay=config['optimizer']['weight_decay'],
+            betas=config['optimizer']['betas'],
+            eps=1e-9
+        )
+
+        scheduler_kwargs = dict(config['scheduler']) | {
+            "steps_per_epoch": len(self.dataloader) * config['dataset']['batch_expand_size'],
+            "epochs": config['trainer']['epochs'],
+            "max_lr": config['trainer']['learning_rate'],
+        }
+
+        self.scheduler = OneCycleLR(self.optimizer, **scheduler_kwargs)
+
+        self.current_step = 0
+        self.logger = WanDBWriter()
+
+    def train_loop(self):
+        epochs = self.config['trainer']['epochs']
+        tqdm_bar = tqdm(epochs * len(self.dataloader) * self.config['dataset']['batch_expand_size']- self.current_step)
+        device = self.device
+
+        for epoch in range(epochs):
+            for i, batchs in enumerate(self.dataloader):
+                # real batch start here
+                for j, db in enumerate(batchs):
+                    self.current_step += 1
+                    tqdm_bar.update(1)
+                    
+                    # logger.set_step(current_step)
+
+                    # Get Data
+                    character = db["text"].long().to(device)
+                    mel_target = db["mel_target"].float().to(device)
+                    duration = db["duration"].int().to(device)
+                    mel_pos = db["mel_pos"].long().to(device)
+                    src_pos = db["src_pos"].long().to(device)
+                    max_mel_len = db["mel_max_len"]
+
+                    # Forward
+                    mel_output, duration_predictor_output = self.model(character,
+                                                                src_pos,
+                                                                mel_pos=mel_pos,
+                                                                mel_max_length=max_mel_len,
+                                                                length_target=duration)
+
+                    # Calc Loss
+                    total_loss, mel_loss, duration_loss = self.Loss(mel_output,
+                                                        duration_predictor_output,
+                                                        mel_target,
+                                                        duration)
+
+                    # Logger
+                    t_l = total_loss.detach().cpu().numpy()
+                    m_l = mel_loss.detach().cpu().numpy()
+                    d_l = duration_loss.detach().cpu().numpy()
+
+                    self.logger.add_scalar("duration_loss", d_l)
+                    self.logger.add_scalar("mel_loss", m_l)
+                    self.logger.add_scalar("total_loss", t_l)
+
+                    # Backward
+                    self.optimizer.zero_grad(set_to_none=True)
+                    total_loss.backward()
+
+                    # Clipping gradients to avoid gradient explosion
+                    torch.nn.utils.clip_grad_norm_(
+                        self.model.parameters(), self.config['trainer']['grad_clip_thresh'])
+                    
+                    self.optimizer.step()
+                    self.scheduler.step()
+
+                    if self.current_step % self.config['trainer']['save_step'] == 0:
+                        torch.save({'model': self.model.state_dict(), 'optimizer': self.optimizer.state_dict(
+                        )}, os.path.join(self.config['trainer']['save_dir'], 'checkpoint_%d.pth.tar' % self.current_step))
+                        print("save model at step %d ..." % self.current_step)
